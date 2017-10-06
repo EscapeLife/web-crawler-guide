8cbfbd91d203331d9e5fe2a3a94adfef63203c76
Escape <240901017@qq.com>
2017-10-06 23:08:24 +0800

    Add downloader.py

--- 3.下载缓存/1.downloader.py
+++ 3.下载缓存/1.downloader.py
@@ -1,19 +1,49 @@
-import urlparse
-import urllib2
-import random
+#!/usr/bin/env python
+# coding:utf-8
+
+"""
+对下载功能增加缓存检测的功能
+"""
+
 import time
-from datetime import datetime, timedelta
+import random
 import socket
+import urllib2
+import urlparse
+from datetime import datetime
 
 
+# 设置默认代理、下载限速、重试次数、超时时间
 DEFAULT_AGENT = 'wswp'
 DEFAULT_DELAY = 5
 DEFAULT_RETRIES = 1
 DEFAULT_TIMEOUT = 60
 
 
+class Throttle:
+    """下载限速设置
+    同一个域下的连接的下载时间间隔需要超过5s，防止网站禁止访问限制
+    """
+    def __init__(self, delay):
+        self.delay = delay
+        self.domains = {}
+
+    def wait(self, url):
+        domain = urlparse.urlsplit(url).netloc
+        last_accessed = self.domains.get(domain)
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
+
+
 class Downloader:
-    def __init__(self, delay=DEFAULT_DELAY, user_agent=DEFAULT_AGENT, proxies=None, num_retries=DEFAULT_RETRIES, timeout=DEFAULT_TIMEOUT, opener=None, cache=None):
+    """下载者
+    负责下载指定url连接，可以设置代理等功能
+    """
+    def __init__(self, delay=DEFAULT_DELAY, user_agent=DEFAULT_AGENT, proxies=None, num_retries=DEFAULT_RETRIES,
+                 timeout=DEFAULT_TIMEOUT, opener=None, cache=None):
         socket.setdefaulttimeout(timeout)
         self.throttle = Throttle(delay)
         self.user_agent = user_agent
@@ -22,38 +52,34 @@ class Downloader:
         self.opener = opener
         self.cache = cache
 
-
     def __call__(self, url):
         result = None
         if self.cache:
             try:
                 result = self.cache[url]
             except KeyError:
-                # url is not available in cache 
+                # 输入的url不在缓存中
                 pass
             else:
-                if self.num_retries > 0 and 500 <= result['code'] < 600:
-                    # server error so ignore result from cache and re-download
+                if self.num_retries > 0 and 500 <= self['code'] < 600:
+                    # 服务器错误和重新下载
                     result = None
         if result is None:
-            # result was not loaded from cache so still need to download
             self.throttle.wait(url)
             proxy = random.choice(self.proxies) if self.proxies else None
             headers = {'User-agent': self.user_agent}
             result = self.download(url, headers, proxy=proxy, num_retries=self.num_retries)
             if self.cache:
-                # save result to cache
                 self.cache[url] = result
         return result['html']
-
 
     def download(self, url, headers, proxy, num_retries, data=None):
         print 'Downloading:', url
         request = urllib2.Request(url, data, headers or {})
         opener = self.opener or urllib2.build_opener()
         if proxy:
-            proxy_params = {urlparse.urlparse(url).scheme: proxy}
-            opener.add_handler(urllib2.ProxyHandler(proxy_params))
+            proxy_param = {urlparse.urlparse(url).scheme: proxy}
+            opener.add_handler(urllib2.ProxyHandler(proxy_param))
         try:
             response = opener.open(request)
             html = response.read()
@@ -64,29 +90,7 @@ class Downloader:
             if hasattr(e, 'code'):
                 code = e.code
                 if num_retries > 0 and 500 <= code < 600:
-                    # retry 5XX HTTP errors
-                    return self._get(url, headers, proxy, num_retries-1, data)
+                    return self.download(url, headers, proxy, num_retries-1, data)
             else:
                 code = None
         return {'html': html, 'code': code}
-
-
-class Throttle:
-    """Throttle downloading by sleeping between requests to same domain
-    """
-    def __init__(self, delay):
-        # amount of delay between downloads for each domain
-        self.delay = delay
-        # timestamp of when a domain was last accessed
-        self.domains = {}
-        
-    def wait(self, url):
-        """Delay if have accessed this domain recently
-        """
-        domain = urlparse.urlsplit(url).netloc
-        last_accessed = self.domains.get(domain)
-        if self.delay > 0 and last_accessed is not None:
-            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
-            if sleep_secs > 0:
-                time.sleep(sleep_secs)
-        self.domains[domain] = datetime.now()
